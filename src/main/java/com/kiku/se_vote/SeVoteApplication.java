c25a4bc629d952331f73efa63ad17a45cb490a44
kiku99 <elsd0326@gmail.com>
2022-09-27 17:28:38 +0900

    mongodb 연결

---
+++ src/main/java/com/kiku/se_vote/SeVoteApplication.java
@@ -0,0 +1,13 @@
+package com.kiku.se_vote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeVoteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeVoteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kiku/se_vote/SeVoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiku.se_vote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeVoteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
